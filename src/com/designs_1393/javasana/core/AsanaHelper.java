42aa45828cae562ad931ff41a1f7dc971080948a
Sean Barag <sjbarag@gmail.com>
2012-05-12 11:13:57 -0400

    Add a few extra test functions
    Creating a task does not work and throws the following exception:
    "org.apache.http.client.HttpResponseException: Object Not Found"

--- src/com/designs_1393/javasana/core/AsanaHelper.java
+++ src/com/designs_1393/javasana/core/AsanaHelper.java
@@ -4,6 +4,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.params.HttpParams;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.http.Header;
@@ -11,6 +13,9 @@ import org.apache.http.Header;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.client.HttpResponseException;
+
+
 
 import java.net.URLEncoder;
 
@@ -19,7 +24,7 @@ public class AsanaHelper
 	private UsernamePasswordCredentials creds;
 	private DefaultHttpClient httpclient = new DefaultHttpClient();
 
-	private final String API_BASE = "https://app.asana.com/api/1.0/";
+	private final String API_BASE = "https://app.asana.com/api/1.0";
 
 	public AsanaHelper( String APIkey )
 	{
@@ -29,12 +34,62 @@ public class AsanaHelper
 	public String getWorkspaces()
 	{
 		try{
-			HttpGet httpget = new HttpGet(API_BASE +"workspaces");
+			HttpGet httpget = new HttpGet(API_BASE +"/workspaces");
 			httpget.addHeader( BasicScheme.authenticate(creds, "US-ASCII", false) );
 
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
 
 			String responseBody = httpclient.execute(httpget, responseHandler);
+			return responseBody;
+		} catch( Exception e ){ e.printStackTrace(); }
+
+		return "";
+	}
+
+	public String getAllProjects()
+	{
+		try{
+			HttpGet httpget = new HttpGet(API_BASE +"/projects");
+			httpget.addHeader( BasicScheme.authenticate(creds, "US-ASCII", false) );
+
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			return responseBody;
+		} catch( Exception e ){ e.printStackTrace(); }
+
+		return "";
+	}
+
+	public String getProjectsInWorkspace( long workspaceID )
+	{
+		try{
+			HttpGet httpget = new HttpGet(API_BASE +"/workspaces/" +workspaceID +"/projects");
+			httpget.addHeader( BasicScheme.authenticate(creds, "US-ASCII", false) );
+
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			return responseBody;
+		} catch( Exception e ){ e.printStackTrace(); }
+
+		return "";
+	}
+
+	public String createTask( String name, long workspaceID )
+	{
+		try{
+			HttpPost httppost = new HttpPost(API_BASE +"/workspace/" +workspaceID +"/tasks");
+			httppost.addHeader( BasicScheme.authenticate(creds, "US-ASCII", false) );
+
+			HttpParams params = httppost.getParams().copy();
+			params.setLongParameter("workspace", workspaceID);
+			params.setParameter("name", name);
+			httppost.setParams( params );
+
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+			String responseBody = httpclient.execute(httppost, responseHandler);
 			return responseBody;
 		} catch( Exception e ){ e.printStackTrace(); }
 
--- test/JavasanaTest.java
+++ test/JavasanaTest.java
@@ -1,11 +1,24 @@
 import com.designs_1393.javasana.core.*;
 
+import java.lang.Long;
+
 public class JavasanaTest
 {
 	public static void main( String[] args )
 	{
 		AsanaHelper ah = new AsanaHelper("REPLACE_WITH_YOUR_API_KEY");
 
-		System.out.println(ah.getWorkspaces());
+		System.out.println("All workspaces:");
+		System.out.println("\t" +ah.getWorkspaces());
+
+		System.out.println("All projects in any workspace:");
+		System.out.println("\t" +ah.getAllProjects());
+
+		System.out.println("All projects in workspace \"1393 Designs\"");
+		System.out.println("\t" +ah.getProjectsInWorkspace( 185645369321L ));
+
+		System.out.println("Create a task with name \"Javasana test\" to workspace \"1393 Designs\"");
+		System.out.println("\t" +ah.createTask( "Javasana test",
+			185645369321L ));
 	}
 }
