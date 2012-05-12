6ca2ab89b93e1c25d6be01f7c69ef8d198f8cabe
Sean Barag <sjbarag@gmail.com>
2012-05-12 10:20:43 -0400

    Simple test of Asana API.
    Prints the JSON response of a user's list of available workspaces.

--- src/com/designs_1393/javasana/core/AsanaHelper.java
+++ src/com/designs_1393/javasana/core/AsanaHelper.java
@@ -1,9 +1,43 @@
 package com.designs_1393.javasana.core;
 
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.Header;
+
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.impl.client.BasicResponseHandler;
+
+import java.net.URLEncoder;
+
 public class AsanaHelper
 {
-	public AsanaHelper()
+	private UsernamePasswordCredentials creds;
+	private DefaultHttpClient httpclient = new DefaultHttpClient();
+
+	private final String API_BASE = "https://app.asana.com/api/1.0/";
+
+	public AsanaHelper( String APIkey )
 	{
-		System.out.println("AsanaHelper constructor called!");
+		creds = new UsernamePasswordCredentials(APIkey, "");
+	}
+
+	public String getWorkspaces()
+	{
+		try{
+			HttpGet httpget = new HttpGet(API_BASE +"workspaces");
+			httpget.addHeader( BasicScheme.authenticate(creds, "US-ASCII", false) );
+
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			return responseBody;
+		} catch( Exception e ){ e.printStackTrace(); }
+
+		return "";
 	}
 }
--- test/JavasanaTest.java
+++ test/JavasanaTest.java
@@ -4,8 +4,8 @@ public class JavasanaTest
 {
 	public static void main( String[] args )
 	{
-		AsanaHelper ah = new AsanaHelper();
+		AsanaHelper ah = new AsanaHelper("REPLACE_WITH_YOUR_API_KEY");
 
-		System.out.println("Hello, World!");
+		System.out.println(ah.getWorkspaces());
 	}
 }
